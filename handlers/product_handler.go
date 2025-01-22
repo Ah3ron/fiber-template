2cd50d10a324f81242a5690979de995595f59717
Ah3ron <ah3ron@protonmail.com>
2025-01-22 11:45:28 +0300

    Update

--- handlers/product_handler.go
+++ handlers/product_handler.go
@@ -14,7 +14,13 @@ func CreateProduct(db *gorm.DB) fiber.Handler {
 			return c.Status(400).SendString(err.Error())
 		}
 		db.Create(product)
-		return c.Redirect("/")
+
+		// Return the updated product list
+		var products []models.Product
+		db.Find(&products)
+		return c.Render("product_list", fiber.Map{
+			"Products": products,
+		})
 	}
 }
 
@@ -32,6 +38,12 @@ func DeleteProduct(db *gorm.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		id := c.Params("id")
 		db.Delete(&models.Product{}, id)
-		return c.Redirect("/")
+
+		// Return the updated product list
+		var products []models.Product
+		db.Find(&products)
+		return c.Render("product_list", fiber.Map{
+			"Products": products,
+		})
 	}
 }
