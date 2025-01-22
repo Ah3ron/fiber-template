010b1effd9cc665a4677f9c46611a557309efb38
Ah3ron <ah3ron@protonmail.com>
2025-01-22 11:33:52 +0300

    Update

--- handlers/product_handler.go
+++ handlers/product_handler.go
@@ -14,7 +14,7 @@ func CreateProduct(db *gorm.DB) fiber.Handler {
 			return c.Status(400).SendString(err.Error())
 		}
 		db.Create(product)
-		return c.JSON(product)
+		return c.Redirect("/")
 	}
 }
 
@@ -22,29 +22,9 @@ func GetAllProducts(db *gorm.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		var products []models.Product
 		db.Find(&products)
-		return c.JSON(products)
-	}
-}
-
-func GetProductByID(db *gorm.DB) fiber.Handler {
-	return func(c *fiber.Ctx) error {
-		id := c.Params("id")
-		var product models.Product
-		db.First(&product, id)
-		return c.JSON(product)
-	}
-}
-
-func UpdateProduct(db *gorm.DB) fiber.Handler {
-	return func(c *fiber.Ctx) error {
-		id := c.Params("id")
-		var product models.Product
-		db.First(&product, id)
-		if err := c.BodyParser(&product); err != nil {
-			return c.Status(400).SendString(err.Error())
-		}
-		db.Save(&product)
-		return c.JSON(product)
+		return c.Render("product_list", fiber.Map{
+			"Products": products,
+		})
 	}
 }
 
@@ -52,6 +32,6 @@ func DeleteProduct(db *gorm.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		id := c.Params("id")
 		db.Delete(&models.Product{}, id)
-		return c.SendString("Product deleted")
+		return c.Redirect("/")
 	}
 }
--- routes/routes.go
+++ routes/routes.go
@@ -12,28 +12,9 @@ func SetupRoutes(app *fiber.App, db *gorm.DB) {
 	app.Static("/static", "./static")
 
 	// HTML routes
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Render("index", fiber.Map{})
-	})
-
-	// HTMX routes
-	app.Get("/api/data", func(c *fiber.Ctx) error {
-		return c.SendString("<p>Data loaded via HTMX!</p>")
-	})
-
-	// User routes
-	userRoutes := app.Group("/users")
-	userRoutes.Post("/", handlers.CreateUser(db))
-	userRoutes.Get("/", handlers.GetAllUsers(db))
-	userRoutes.Get("/:id", handlers.GetUserByID(db))
-	userRoutes.Put("/:id", handlers.UpdateUser(db))
-	userRoutes.Delete("/:id", handlers.DeleteUser(db))
+	app.Get("/", handlers.GetAllProducts(db))
 
 	// Product routes
-	productRoutes := app.Group("/products")
-	productRoutes.Post("/", handlers.CreateProduct(db))
-	productRoutes.Get("/", handlers.GetAllProducts(db))
-	productRoutes.Get("/:id", handlers.GetProductByID(db))
-	productRoutes.Put("/:id", handlers.UpdateProduct(db))
-	productRoutes.Delete("/:id", handlers.DeleteProduct(db))
+	app.Post("/products", handlers.CreateProduct(db))
+	app.Delete("/products/:id", handlers.DeleteProduct(db))
 }
